f423edfe52a03d8984436898cab6a6ea466e837e
Mohammad Maroof <mmaroof487@gmail.com>
2025-01-10 14:52:14 +0530

    feat: homepage

--- client/src/pages/Intro/index.jsx
+++ client/src/pages/Intro/index.jsx
@@ -8,9 +8,9 @@ export default function Intro() {
 				</div>
 			</div>
 			<div className="w-full flex justify-center mt-8">
-				<div className="rounded-full border-2 border-neon-green  w-1/3">
+				<div className="rounded-full border-2 border-neon-green  w-1/3 flex p-2">
 					<input type="text" className="outline-none bg-transparent  px-4 py-2  w-full focus:outline-none" />
-					<input type="button" value="Join Wait list" className="rounded-full bg-neon-green" />
+					<input type="button" value="Join Wait list" className="rounded-full bg-neon-green px-4" />
 				</div>
 			</div>
 		</>
